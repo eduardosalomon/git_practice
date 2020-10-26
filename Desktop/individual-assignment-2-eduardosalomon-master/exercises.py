b8d5fff593ad03444e88176af6cc01a740f219fc
Eduardo Salomon <esalomon@student.ie.edu>
2020-10-26 21:07:02 +0100

    finished exercise 1

---
+++ Desktop/individual-assignment-2-eduardosalomon-master/exercises.py
@@ -0,0 +1,33 @@
+
+form = {
+        "password": "password"
+    }
+
+
+def validate_form(form):
+    if "email" not in form:
+        raise KeyError("key \"email\" not present in form")
+    if type(form["email"]) is not str:
+        raise TypeError("email should be a string")
+    if "@" not in form["email"] or "." not in form["email"]:
+        raise ValueError("invalid email")
+
+    return form
+
+# ASSINGMENT 1
+def handle_errors_validate_form(form):
+    try:
+        return validate_form(form)
+    except KeyError:
+            return("key not present in the form")
+    except ValueError:
+            return("there's something wrong about the values in your form")
+    except TypeError:
+            return("there's something wrong about the types in your form")
+
+print(handle_errors_validate_form(form))
+    
+
+
+
+
